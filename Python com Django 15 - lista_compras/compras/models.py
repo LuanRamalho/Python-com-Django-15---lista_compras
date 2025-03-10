1af9f67b3fab0056eb9fbeb4a508bded722a1747
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-03-10 15:58:11 -0300

    models.py

--- Python com Django 15 - lista_compras/compras/models.py
+++ Python com Django 15 - lista_compras/compras/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Nota(models.Model):
     titulo = models.CharField(max_length=255)
+    anotacao_precos = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.titulo
